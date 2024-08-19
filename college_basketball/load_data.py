3c0bd6bcf5db577ad812b2afc75b66e2723ec49e
jsachs802 <80415257+jsachs802@users.noreply.github.com>
2024-08-19 19:10:00 -0400

    Add files via upload

---
+++ college_basketball/load_data.py
@@ -0,0 +1,35 @@
+from get_team_functions_adv import parse_table
+from get_team_data import headers
+from get_team_data import team_url
+import pandas as pd
+import json
+import time
+
+# NOTE: should provide full paths 
+
+def get_data(pad: float):
+    """
+    Master function for requesting data. 
+    Collects tables for each team and returns 
+    a large dataframe.
+    
+    :param pad - float - number of seconds to sleep in between calls
+    """
+    # Load team_dict
+    with open('/Users/jeffreysachs/Documents/ncaa_predictor/team_dict.json', 'r', encoding='utf-8') as testfile:
+        team_dict = json.load(testfile)
+
+    # Iterate and grab tables from each team 
+    team_dfs = []
+    for key, value in team_dict.items():
+        team_dfs.append(parse_table(headers=headers, team_dict=team_dict, team_name=key, base_url=team_url))
+        time.sleep(pad)
+
+    # Combine list into single table
+    all_team_data = pd.concat(team_dfs, ignore_index=True)
+
+    # Save large table as csv
+    all_team_data.to_csv('/Users/jeffreysachs/Documents/ncaa_predictor/data/all_team_data.csv', encoding='UTF-8', index=False)
+    
+
+
